ff689cd5fa47a7616ad2a690cfeab53d40a455b1
xinr <hcyhsh@gmail.com>
2024-03-07 01:21:05 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx878c4bc2aa14e9c9",
   appsecret: "432e9c2c3f16306d43feb953c896dabc",
-  template_id: "DpRUV0Zq5G3zqlBy3oycuw-3gD7vrg3BVHSfKg7-qgA",
+  template_id: "9FnSyv2KJX_cuqF3e9LyHmFVpQpKhJxc_i54N_X8YPM",
   touser: "oxV5_6FMFlMiM_ym2viFiJD5paPU",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
