ce0b4acd145bb17ff9059519fb9331d245690916
xinr <hcyhsh@gmail.com>
2024-03-07 01:15:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx878c4bc2aa14e9c9",
+  appsecret: "432e9c2c3f16306d43feb953c896dabc",
+  template_id: "DpRUV0Zq5G3zqlBy3oycuw-3gD7vrg3BVHSfKg7-qgA",
+  touser: "oxV5_6FMFlMiM_ym2viFiJD5paPU",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "广州",
+  first_date: "2024-02-14",
 };
